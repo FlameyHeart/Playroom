47875fcdd0e66075508d60c8d8bb62b026bc6b2d
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2023-07-13 01:03:27 -0300

    Initial commit

---
+++ src/client/java/io/github/flameyheart/playroom/PlayroomClient.java
@@ -0,0 +1,10 @@
+package io.github.flameyheart.playroom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlayroomClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/io/github/flameyheart/playroom/PlayroomDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.flameyheart.playroom;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PlayroomDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
